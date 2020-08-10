e4ab286a3e2fd406e29933844de5826315b2e633
nam-web <65065658+nam-web@users.noreply.github.com>
2020-08-10 19:14:02 +0530

    my files

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$con = mysqli_connect('localhost','root','nimmo1130','my_db');
+$txtName = $_POST["txtName"];
+$txtBranch = $_POST["txtBranch"];
+$txtYear = $_POST["$txtYear"];
+$txtQuestion1 = $_POST["txtQuestion1"];
+
+$sql = "INSERT INTO response_tb (name,branch,year,question1) VALUES ('$txtName', '$txtBranch', '$txtYear', '$txtQuestion1')";
+
+$rs = mysqli_query($con, $sql);
+
+if($rs)
+{
+  echo "Contact Records Inserted";
+}
+?>
