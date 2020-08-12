d2bc97878c3ad193816286a406d0cdbe668d4ba7
nam-web <65065658+nam-web@users.noreply.github.com>
2020-08-12 13:37:37 +0530

    ab ho gaya

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect('nam-web.github.io', 'root', 'nimmo1130', 'my_db');
+$con = mysqli_connect('https://nam-web.github.io/trail1/', 'root', 'nimmo1130', 'my_db');
 $txtName = $_POST["txtName"];
 $txtBranch = $_POST["txtBranch"];
 $txtYear = $_POST["$txtYear"];
